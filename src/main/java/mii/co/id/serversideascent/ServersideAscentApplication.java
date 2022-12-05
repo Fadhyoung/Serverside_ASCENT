e6a56dae2622fa2da4499d2002ccf3cda4459e35
fadhli <fadhlinurhimawan@gmail.com>
2022-12-05 20:34:43 +0700

    first commit

---
+++ src/main/java/mii/co/id/serversideascent/ServersideAscentApplication.java
@@ -0,0 +1,13 @@
+package mii.co.id.serversideascent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideAscentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersideAscentApplication.class, args);
+	}
+
+}
